9d89b16e643e20b4cd7cd5086c1bf2cd95303bff
Pavithra V <132774459+Pavithrav2005@users.noreply.github.com>
2025-05-23 10:42:28 +0530

    Update scheduler.py

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -34,7 +34,7 @@ def schedule_meetings(names_from_user, date, time, days):
         ).strftime("%Y-%m-%d")
 
         for email in emails:
-            base_url = "http://103.114.208.197:5000/api"
+            base_url = "http://103.114.208.197:5001/api"
             accept_link = f"{base_url}/rsvp/accept/{email}"
             decline_link = f"{base_url}/rsvp/decline/{email}"
 
